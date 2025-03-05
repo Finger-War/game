9903fcc9a7f887093a438dcf2f209280d60bb858
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2025-03-04 22:45:13 -0300

    chore: add assets on build script

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 mix deps.get --only prod
 MIX_ENV=prod mix compile
-MIX_ENV=prod mix phx.digest
+MIX_ENV=prod mix assets.build
+MIX_ENV=prod mix assets.deploy
 MIX_ENV=prod mix phx.gen.release
 MIX_ENV=prod mix release --overwrite
